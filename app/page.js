221b21c0ba08373d9f454608d7d4c45c4472d4be
Himanshu Maurya <him2000.maurya@gmail.com>
2025-01-28 22:31:44 +0530

    adding the userAuthentication

--- app/page.js
+++ app/page.js
@@ -2,8 +2,11 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
+    <div className="mt-40">
+
 <Button variant='destructive'>
   hell don't touch the button
 </Button>
+    </div>
   );
 }
